bd03d4cf15bf7fe0e2a8c474bb6ed908aca661a2
mohamed9974 <e204472@metu.edu.tr>
2023-03-15 10:55:33 +0300

    added the new configs

--- start_cluster.sh
+++ start_cluster.sh
@@ -5,32 +5,31 @@
 # docker registry
 
 # start the cluster
-microk8s start
 
-# enable the registry
-sudo microk8s enable registry:size=20Gi
+## enable the registry
+#sudo microk8s enable registry:size=20Gi
 
-# enable the dns
-microk8s enable dns
+## enable the dns
+#microk8s enable dns
 
-# enable the dashboard
-microk8s enable dashboard
+## enable the dashboard
+#microk8s enable dashboard
 
-# enable the storage
-microk8s enable storage
+## enable the storage
+#microk8s enable storage
 
-# enable the ingress
-microk8s enable ingress
+## enable the ingress
+#microk8s enable ingress
 
-# enable the metrics server
-microk8s enable metrics-server
+## enable the metrics server
+#microk8s enable metrics-server
 
-# 
-microk8s.kubectl config view --raw > $HOME/.kube/config
+## 
+#microk8s.kubectl config view --raw > $HOME/.kube/config
 
-# create the namespace iotstack
-microk8s kubectl create namespace iotstack
-
+## create the namespace iotstack
+kubectl create namespace iotstack
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
 # check if skaffold is installed and install it if not
 if ! command -v skaffold &> /dev/null
 then
@@ -42,13 +41,13 @@ then
 fi
 
 # run skaffold to build the application and check for errors
-skaffold build --default-repo localhost:32000
+skaffold build
 
 # run skaffold to deploy the application to the namespace iotstack and check for errors 
-skaffold run --namespace iotstack --default-repo localhost:32000 --port-forward --status-check
+skaffold dev --namespace iotstack --status-check
 
 # check if the application is running
 kubectl get pods --namespace iotstack
 
 # run the following command to get the ip address of the ingress
-microk8s kubectl get ingress --namespace iotstack
+# microk8s kubectl get ingress --namespace iotstack
